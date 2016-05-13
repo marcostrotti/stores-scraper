6154ad53680f9f0eba8c602c874e14fc927e0f51
Marcos H. Trotti <trotti@econ.unicen.edu.ar>
2016-05-13 16:54:07 -0300

    Demo almacenando algunos datos para estadisticas futuras

--- app.js
+++ app.js
@@ -3,8 +3,40 @@
  **/
  
  var gplay = require('google-play-scraper');
+ var mysql      = require('mysql');
+ var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'scraper',
+    password : '',
+    database : 'scraper'
+ });
+
  
- 
+function getAppInformation(appEntry){
+    
+    gplay.app({appId: appEntry.appId})
+    .then(function(app){
+        console.log('Retrieved application: ' + app.appId);
+    })
+    .catch(function(e){
+        console.log('There was an error fetching the application!');
+    }); 
+}
+
+function updateOrInsertApp(appEntry){
+    var escapedAppId=connection.escape(appEntry.appId);
+    var escapedTitle=connection.escape(appEntry.title);
+    var escapedDeveloper=connection.escape(appEntry.developer);
+    var strQuery= "INSERT INTO apps (appId, title, developer) VALUES ("+escapedAppId+", "+escapedTitle+", "+escapedDeveloper+") " + 
+    " ON DUPLICATE KEY UPDATE title="+escapedTitle+" , developer="+escapedDeveloper;
+    connection.query(strQuery, function(err, rows, fields) {
+        if (err) 
+            console.log('[Erroor] '+ err);
+    });  
+    
+}
+
+
 function scrapCategory(aCategory,aCollection){
     gplay.list({
         category: aCategory,
@@ -12,16 +44,11 @@ function scrapCategory(aCategory,aCollection){
         num: 120
     })
     .then(function(apps){
-        console.log('Retrieved ' + apps.length + ' applications!');
-        apps.forEach(function(appEntry) {
-            gplay.app({appId: appEntry.appId})
-                .then(function(app){
-                console.log('Retrieved application: ' + app.appId);
-            })
-            .catch(function(e){
-                console.log('There was an error fetching the application!');
-            });
+        
+        apps.forEach(function(appEntry) {    
+            updateOrInsertApp(appEntry);
         });
+       // connection.end(function(err) { if(err) console.error('Error On DB Close.'); });
     })
     .catch(function(e){
         console.log('There was an error fetching the list!');
@@ -29,4 +56,12 @@ function scrapCategory(aCategory,aCollection){
 }
 
 
-scrapCategory(gplay.category.GAME_ACTION,gplay.collection.TOP_FREE);
+// Search all categories and collections
+connection.connect();
+//scrapCategory(gplay.category.ANDROID_WEAR,gplay.collection.TOP_FREE);
+for (var i in gplay.category) {
+    for (var j in gplay.collection){
+        console.log("Scraping ", gplay.category[i]," collection ", gplay.collection[j]); 
+        scrapCategory(gplay.category[i],gplay.collection[j]);
+    }
+}
