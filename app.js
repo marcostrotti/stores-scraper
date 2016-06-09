9446dec74dd9b9d02ee414209aff9c60ce1d6678
Marcos H. Trotti <trotti@econ.unicen.edu.ar>
2016-06-09 14:56:30 -0300

    Se trata la excepción por conexión perdida que evitaba la finalización del proceso

--- app.js
+++ app.js
@@ -8,12 +8,7 @@
  var mysql = require('mysql');
  
 
- var conn = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'scraper',
-    password : '',
-    database : 'scraper'
- });
+ var conn;
  
 
  
@@ -69,7 +64,32 @@ function scrapCategory(aCategory,aCollection){
 }
 
 // Search all categories and collections
-conn.connect();
+function handleDisconnect() {
+  conn = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'scraper',
+    password : '',
+    database : 'scraper'
+    });
+
+  conn.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  conn.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect(); 
 var pendingFunctions=[];
 /**
  * TODO: improve to provent socker hang up when query
